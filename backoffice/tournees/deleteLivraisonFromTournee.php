16cdeb6c93c009a79d56b14552d4c55ec61e20eb
SandrinePatin <sardine0099@gmail.com>
2019-06-18 12:30:09 +0200

    Delete Tournee

--- backoffice/tournees/deleteLivraisonFromTournee.php
+++ backoffice/tournees/deleteLivraisonFromTournee.php
@@ -4,14 +4,26 @@ require_once __DIR__ . '/../../includes.php';
 
 if (isset($_POST['idLivraison']) === true) {
     $livraison = $_POST['idLivraison'];
+    cancelLivraison($livraison);
+}
+elseif (isset($_POST['idTournee']) === true){
+    $tournee=$_POST['idTournee'];
+var_dump($tournee);
+    $livraisonsToCancel=getLivraisonByIdTournee($tournee);
+    foreach ($livraisonsToCancel as $livraison){
+        cancelLivraison($livraison['identifiant_livraison']);
+    }
+}
+
+function cancelLivraison($livraison){
     setLivraisonEtat($livraison, 'canceled');
 
     //unset products form Livraison
     $productsLivraison = getProductByLivraisonId($livraison);
+    var_dump($productsLivraison);
     if (empty($productsLivraison) !== true) {
         foreach ($productsLivraison as $product) {
             updateProductSetLivraison($product['identifiant'], NULL);
         }
     }
 }
-
--- backoffice/tournees/tourneesDisplay.js
+++ backoffice/tournees/tourneesDisplay.js
@@ -51,7 +51,16 @@ function setEtatLivraison(idlivraison, etatToSet) {
 }
 
 function deleteTournee(idTournee) {
+    const request = new XMLHttpRequest();
+    request.onreadystatechange = function () {
+        if (request.readyState === 4) {
 
+            console.log(request.responseText);
+        }
+    };
+    request.open('POST', '../backoffice/tournees/deleteLivraisonFromTournee.php');
+    request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    request.send(`idTournee=${idTournee}`);
 }
 
 function hide(element) {
